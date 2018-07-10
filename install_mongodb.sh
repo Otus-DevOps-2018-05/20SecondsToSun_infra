d35de80b7edb58629aaaba09ca50e781bbf43ac2
Yuri Popov <yurikblech@gmail.com>
2018-07-10 23:24:45 +0300

    mongodb bug fix

--- install_mongodb.sh
+++ install_mongodb.sh
@@ -8,7 +8,3 @@ sudo apt update
 sudo apt install -y mongodb-org
 sudo systemctl start mongod
 sudo systemctl enable mongod
-
-git clone -b monolith https://github.com/express42/reddit.git
-cd reddit && bundle install
-puma -d
